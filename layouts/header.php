6f4ce1cc6c64a0d6871f62e9725d9ccf443286d9
tipnipa <tipnipa2020@gmail.com>
2023-02-28 20:11:37 +0700

    scs211

---
+++ layouts/header.php
@@ -0,0 +1,15 @@
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+      <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Features</a>
+        <a class="p-2 text-dark" href="#">Enterprise</a>
+        <a class="p-2 text-dark" href="#">Support</a>
+        <a class="p-2 text-dark" href="#">Pricing</a>
+      </nav>
+      <a class="btn btn-outline-primary" href="#">Sign up</a>
+    </div>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Teacher&student</h1>
+      <p class="lead d-none">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
+    </div>
---
+++ teacher.php
@@ -0,0 +1,99 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Teacher</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/pricing/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.3/examples/pricing/pricing.css" rel="stylesheet">
+  </head>
+  <body>
+  <?php include("layouts/header.php") ?>
+
+    <div class="container">
+      <div class="text-center">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+        			<th>#</th>
+              <th>ชื่อสกุล</th>
+              <th>รายละเอียด</th>
+        		</tr>
+          </thead>
+        	<tbody>
+            <tr>
+        			<td>1</td>
+        			<td>อาจารย์สุนี ปัญจะเทวคุปก์ </td>
+        			<td><a href="#">คลิก</a></td>
+        		</tr>
+            <tr>
+        			<td>2</td>
+        			<td>อาจารย์วิวัฒน์ ชินนาทศิริกุล์</td>
+        			<td><a href="#">คลิก</a></td>
+        		</tr>
+            <tr >
+        			<td>3 </td>
+        			<td>อาจารย์ดาวรถา วีระพันธ์</td>
+        			<td><a href="#">คลิก</a></td>
+        		</tr>
+            <tr>
+        			<td>4</td>
+        			<td>อาจารย์ณัฎฐิรา ศุขไพบูลย์์</td>
+        			<td><a href="#">คลิก</a></td>
+        		</tr>
+            <tr>
+        			<td>5 </td>
+              <td>อาจารย์ประณมกร อัมพรพรรดิ์</td>
+        			<td><a href="#">คลิก</a></td>
+        		</tr>
+            <tr>
+        			<td>6 </td>
+        			<td>อ่าจารย์วิศรุต ขวัญคุ้ม</td>
+        			<td><a href="#">คลิก</a></td>
+        		</tr>
+            <tr>
+        			<td>7 </td>
+        			<td>อาจารย์ณัฐรดี อนุพงค์ </td>
+        			<td><a href="#">คลิก</a></td>
+        		</tr>
+            <tr>
+        			<td>8 </td>
+        			<td>อาจารย์ชวลิต โควีระวงศ์</td>
+        			<td><a href="#">คลิก</a></td>
+        		</tr>
+
+        	</tbody>
+        </table>
+      </div>
+
+      <?php include("layouts/footer.php") ?>
+    </div>
+  </body>
+</html>
